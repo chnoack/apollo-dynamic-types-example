a882879166d97ed8188d4e7625a297bbc0735fef
Christian Noack <christian.noack@agile-methoden.de>
2020-11-25 11:28:03 +0100

    cleanup

--- src/graphql/pubsub.ts
+++
@@ -1,9 +0,0 @@
-// ----- Uncomment for default pubsub implementation -----
-import { PubSub } from 'apollo-server';
-export const pubsub = new PubSub();
-// -------------------------------------------------------
-
-// ------ Uncomment for Redis pubsub implementation ------
-// import { RedisPubSub } from 'graphql-redis-subscriptions';
-// export const pubsub = new RedisPubSub();
-// -------------------------------------------------------
